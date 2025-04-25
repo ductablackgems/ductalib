a97bf551766a32150fc664cdac050b80dd57291a
ductablackgems <ducta@blackgemsglobal.com>
2025-04-25 17:55:44 +0700

    log

--- Scripts/Common/LogHelper.cs
+++ Scripts/Common/LogHelper.cs
@@ -7,28 +7,20 @@ namespace _0.DucLib.Scripts.Common
         // ReSharper disable Unity.PerformanceAnalysis
         public static void LogYellow(string mess)
         {
-#if DEBUG_NET
             Debug.Log($"<color=yellow>[LOG]{mess}</color>");
-#endif
         }
         public static void LogPurple(string mess)
         {
-#if DEBUG_NET
             Debug.Log($"<color=#FF00E6>[LOG]{mess}</color>");
-#endif
         }
         public static void LogError([System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
-#if DEBUG_NET
             Debug.Log($"<color=red>[CHECK POINT]-[{memberName}]</color>");
-#endif
         }
         // ReSharper disable Unity.PerformanceAnalysis
         public static void CheckPoint([System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
-#if DEBUG_NET
             Debug.Log($"<color=yellow>[CHECK POINT]-[{memberName}]</color>");
-#endif
         }
         public static void Todo(string todo,[System.Runtime.CompilerServices.CallerMemberName] string memberName = "")
         {
