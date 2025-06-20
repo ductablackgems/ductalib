91a37c25e0002dc8b4b47b5af584f4c442677e11
ductablackgems <ducta@blackgemsglobal.com>
2025-06-20 15:15:27 +0700

    confirm reward popup

---
+++ Scripts/Ads/ConfirmRewardPopup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using _0.DucLib.Scripts.Common;
+using _0.DucTALib.Scripts.Common;
+using UnityEngine;
+
+namespace _0.DucLib.Scripts.Ads
+{
+    public class ConfirmRewardPopup : MonoBehaviour
+    {
+        public List<Transform> content;
+        public Action completeCallback;
+        private Transform currentContent;
+        public void Show(int id, Action callback)
+        {
+            currentContent = content[id];
+            currentContent.ShowObject();
+            currentContent.ScaleInPopup();
+            completeCallback = callback;
+        }
+
+        public void Confirm()
+        {
+            CallAdsManager.ShowRewardVideo("",() =>
+            {
+                currentContent.HideObject();
+                completeCallback?.Invoke();
+                completeCallback = null;
+                currentContent = null;
+                gameObject.HideObject();
+            });
+        }
+
+        public void Cancel()
+        {
+            currentContent.HideObject();
+            completeCallback = null;
+            currentContent = null;
+            gameObject.HideObject();
+        }
+    }
+}
