a57b66f190e0c08bd34866a3b9743210504703f9
ductablackgems <ducta@blackgemsglobal.com>
2025-07-11 15:42:52 +0700

    1

---
+++ Character/Scripts/D_CharacterAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace _0.DucTALib.Character.Scripts
+{
+    public class D_CharacterAnimation : CharacterAnimation
+    {
+        public Animator animator;
+
+        public void PlayIdle()
+        {
+            animator.Play("Idle");
+        }
+
+        public void PlayWalk()
+        {
+            animator.Play("walk");
+        }
+
+        public void PlayRun()
+        {
+            animator.Play("Run");
+        }
+    }
+}
