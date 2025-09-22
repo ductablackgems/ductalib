1b3c1e0e42a5dbb0d349c6e06b8828d4f72d794c
DucTA <ducta@blackgemsglobal.com>
2025-09-22 10:13:08 +0700

    1

---
+++ Splash/Scripts/SimpleSplash.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using _0.DucLib.Scripts.Ads;
+using _0.DucTALib.Scripts.Common;
+using _0.DucTALib.Scripts.Loading;
+using BG_Library.NET;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _0.DucTALib.Splash.Scripts
+{
+    public class SimpleSplash : SingletonMono<SimpleSplash>
+    {
+        private float loadDuration;
+        private float currentTime;
+        private float currentProgress;
+        private float targetProgress;
+        private float stopTimer = 0f;
+        private float smoothSpeed = 0.02f;
+        private int currentMessageIndex;
+        public GameObject loading;
+        public Image loadingBar;
+        public TextMeshProUGUI loadingText;
+        public TextMeshProUGUI currentProgressTxt;
+        [ReadOnly] public string[] loadingTxt = new string[]
+        {
+            "Checking network connection...",
+            "Loading user profile data...",
+            "Initializing game assets...",
+            "Fetching remote configurations...",
+            "Initializing Firebase services...",
+            "Loading ads configuration...",
+            "Preparing game environment...",
+            "Syncing player data...",
+            "Setting up audio manager...",
+            "Loading game resources...",
+            "Initializing leaderboard...",
+            "Establishing server connection...",
+            "Loading in-game tutorials...",
+            "Setting up user preferences...",
+            "Finalizing game setup..."
+        };
+        private void Start()
+        {
+            SplashTracking.loading_duration.Reset();
+            SplashTracking.loading_duration.Start();
+            StartCoroutine(AdsControl());
+            StartCoroutine(WaitToLoadScene());
+        }
+        
+        
+        private IEnumerator AdsControl()
+        {
+            yield return new WaitUntil(() => CommonRemoteConfig.instance.fetchComplete);
+            CallAdsManager.LoadInterByGroup("bg_fa_launch");
+        }
+        
+        private IEnumerator WaitToLoadScene()
+        {
+            yield return new WaitForEndOfFrame();
+
+            float fbTimeout = 5;
+            while (fbTimeout > 0 && (RemoteConfig.Ins == null || RemoteConfig.Ins.isDataFetched))
+            {
+                fbTimeout -= Time.deltaTime;
+                yield return null;
+            }
+
+            loadDuration = RemoteConfig.Ins.isDataFetched
+                ? CommonRemoteConfig.instance.splashConfig.timeoutMin
+                : 15f;
+            loadingBar.fillAmount = 0;
+            currentProgressTxt.text = $"0%";
+            while (currentTime < loadDuration)
+            {
+                HandleLoadingProgress();
+                yield return null;
+            }
+
+            FinishLoadingPhase();
+            AppOpenCaller.IgnoreAppOpenResume = true;
+            SplashTracking.SetUserProperty();
+            CallAdsManager.ShowInter("bg_fa_launch");
+            FinishLoading();
+            
+        }
+        private void FinishLoadingPhase()
+        {
+            loadingText.text = "Starting game...";
+            loadingBar.DOFillAmount(1, 0.5f);
+            currentProgressTxt.text = "100%";
+        }
+
+        private void HandleLoadingProgress()
+        {
+            int canStop = Random.Range(0, 4);
+            if (stopTimer <= 0f && canStop == 3)
+            {
+                float speedModifier = Random.Range(0.001f, 0.01f);
+                currentProgress = Mathf.MoveTowards(currentProgress, targetProgress, speedModifier);
+                if (Random.value > 0.9f)
+                    stopTimer = Random.Range(0.35f, 0.85f);
+            }
+            else
+            {
+                stopTimer -= Time.deltaTime;
+            }
+
+            loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, currentProgress, smoothSpeed);
+            currentProgressTxt.text = $"{(int)(loadingBar.fillAmount * 100)}%";
+
+            if (currentProgress >= (float)(currentMessageIndex + 1) / loadingTxt.Length &&
+                currentMessageIndex < loadingTxt.Length - 1)
+            {
+                currentMessageIndex++;
+                loadingText.text = loadingTxt[currentMessageIndex];
+            }
+
+            currentTime += Time.deltaTime;
+            targetProgress = Mathf.Clamp01(currentTime / loadDuration);
+        }
+
+        private void FinishLoading()
+        {
+            CallAdsManager.LoadInterByGroup("gameplay");
+            CallAdsManager.LoadInterByGroup("break");
+            LoadingScene.instance.LoadMenu();
+            AppOpenCaller.IgnoreAppOpenResume = false;
+        }
+    }
+}
