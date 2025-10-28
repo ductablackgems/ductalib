9031aa5bd9671b58f628794807a3345eaf636201
DucTA <ducta@blackgemsglobal.com>
2025-10-28 09:46:38 +0700

    1

--- Scripts/Ads/Immersive/ImmersiveAdObject.cs
+++ Scripts/Ads/Immersive/ImmersiveAdObject.cs
@@ -6,18 +6,30 @@ namespace _0.DucLib.Scripts.Ads.Immersive
     public class ImmersiveAdObject : MonoBehaviour
     {
         public string pos;
-
-        
+        public bool showInStart;
 
 #if USE_IMMERSIVE_ADMOB
         private void Start()
         {
-            ShowAds();
+            if (showInStart)
+                ShowAds();
         }
+
         public void ShowAds()
         {
             CallAdsManager.ShowImmersive(pos, this.gameObject);
         }
+
+        public void DestroyAd()
+        {
+            CallAdsManager.DestroyImmersive(pos);
+        }
+        public void LoadAds()
+        {
+            CallAdsManager.InitImmersive(pos);
+        }
+        
 #endif
+        
     }
 }
