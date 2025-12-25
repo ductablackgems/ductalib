cb36cb7799d7363fb1d182337e9ce8d3e22a9815
DucTA <ducta@blackgemsglobal.com>
2025-12-25 10:46:10 +0700

    1

---
+++ Splash/Intro/Scenes/IntroDemoManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace _0.DucTALib.Splash.Intro.Scenes
+{
+    public class IntroDemoManager : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Splash/Intro/Scripts/IntroController.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _0.DucTALib.Splash.Intro.Scripts
+{
+    public class IntroController : SingletonMono<IntroController>
+    {
+        private void Start()
+        {
+            
+        }
+    }
+}
